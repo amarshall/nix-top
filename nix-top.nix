25533920d5b880e7b3af7801d28bf9b4709cddc3
Daiderd Jordan <daiderd@gmail.com>
2018-07-30 03:36:33 +0200

    add workaround for stty on darwin
    Using stty from coreutils results in error messages, I'm not sure what
    the problem is here but using the system version seems to be the only
    workaround.
    
    stty: 'standard input': unable to perform all requested operations.

--- nix-top.nix
+++ nix-top.nix
@@ -26,10 +26,12 @@ stdenv.mkDerivation rec {
   ADDITIONAL_PATH = lib.makeBinPath [ getent ncurses binutils-unwrapped coreutils findutils ];
 
   installPhase = ''
-    mkdir -p $out/bin/
+    mkdir -p $out/bin $out/libexec/nix-top
     cp ./nix-top $out/bin/nix-top
     chmod +x $out/bin/nix-top
     wrapProgram $out/bin/nix-top \
-      --prefix PATH : "${ADDITIONAL_PATH}"
+      --prefix PATH : "$out/libexec/nix-top:${ADDITIONAL_PATH}"
+  '' + stdenv.lib.optionalString stdenv.isDarwin ''
+    ln -s /bin/stty $out/libexec/nix-top
   '';
 }
